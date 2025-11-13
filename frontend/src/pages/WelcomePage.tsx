1665183b0816325686ba5827ff12e5004a521a1c
jewelz-code <juliexoxo08@gmail.com>
2025-11-13 16:15:58 -0500

    For Later

---
+++ frontend/src/pages/WelcomePage.tsx
@@ -0,0 +1,9 @@
+export default function WelcomepPage() {
+    return (
+    <div>
+        <h1 className="text-5xl font-bold text-white mb-4">Balance</h1>
+        <p className="text-lg text-white/90 mb-12 max-w-xl">
+        Your companion for academic success and mental well-being</p>
+    </div>
+    );
+}
